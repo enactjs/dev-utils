23ca65e6a94c23084519c2c986fe39f1d5e9648e
Jason Robitaille <robitaille88@gmail.com>
2017-11-03 15:21:03 -0700

    ENYO-4712:  Unify PrerenderPlugin and LocaleHtmlPlugin (#5)
    * Improved browserslist handling with Electron target. Fixed error when non-themed moonstone-less app screentypes detection.
    
    * Unify PrerenderPlugin and LocaleHtmlPlugin into a single general-purpose plugin
    
    Reviewed-By: Roy Sutton (roy.sutton@lge.com)
    Integrated-By: Ryan Duffy (ryan.duffy@lge.com)

---
+++ plugins/PrerenderPlugin/templates.js
@@ -0,0 +1,85 @@
+const fn = (js) => js && `
+		(function() {
+			${js.replace(/\n/g, '\n\t\t')}
+		})();
+	`;
+
+const startup = (screenTypes, jsAssets) => `
+	// Initialize font scaling for resolution independence.
+	var screenTypes = ${JSON.stringify(screenTypes)} || [];
+	var defaultType = {name: 'standard', pxPerRem: 16, width: window.innerWidth, height: window.innerHeight, aspectRatioName: 'standard', base: true};
+	if(screenTypes.length===0) {
+		screenTypes.push(defaultType);
+	}
+	var height = window.innerHeight,
+		width = window.innerWidth;
+	var scrObj = screenTypes[screenTypes.length - 1];
+	if(height > width) {
+		width = height;
+	}
+	for(var i=screenTypes.length-1; i>=0; i--) {
+		if(width <= screenTypes[i].width) {
+			scrObj = screenTypes[i];
+		}
+	}
+	document.documentElement.style.fontSize = scrObj.pxPerRem + 'px';
+
+	window.onload = function() { setTimeout(function() {
+		if(typeof App === 'undefined') {
+			// Add script nodes, loading the chunks sequentially.
+			var appendScripts = function(js) {
+				if(js.length>0) {
+					var src = js.shift();
+					var script = document.createElement('script');
+					script.type = 'text/javascript';
+					script.src = src;
+					script.onload = function() {
+						appendScripts(js);
+					};
+					document.body.appendChild(script);
+				}
+			};
+			appendScripts(${JSON.stringify(jsAssets)});
+		} else {
+			// V8 snapshot, so update the javascript environment then render.
+			if(typeof updateEnvironment === 'function') {
+				updateEnvironment();
+			}
+			if(typeof App === 'object' && (typeof ReactDOM === 'object')) {
+				ReactDOM.render(App['default'] || App, document.getElementById('root'));
+			} else {
+				console.log('ERROR: Snapshot app not found');
+			}
+		}
+	}, 0); };
+`;
+
+const deepLink = (conditions, fallback) => conditions ? `
+	// Handle any deep link conditions.
+	if(${(Array.isArray(conditions) ? conditions.join(' && ') : conditions)}) {
+		var div = document.getElementById("root");
+		while(div && div.firstChild) { div.removeChild(div.firstChild); }
+	${fallback ? `} else {
+		${fallback.replace(/\n/g, '\n\t')}
+	}` : '}'}
+` : (fallback || null);
+
+const multiLocale = (mapping) => mapping && `
+	// Apply locale-specific root classes and checksum.
+	var details = ${JSON.stringify(mapping, null, '\t').replace(/\n/g, '\n\t')};
+	var lang = navigator.language.toLowerCase();
+	var conf = details[lang] || details[lang.substring(0, 2)];
+	var reactRoot = document.getElementById("root").children[0];
+	if(conf && reactRoot) {
+		reactRoot.className += conf.classes;
+		reactRoot.setAttribute("data-react-checksum", conf.checksum);
+	}
+`;
+
+module.exports = {
+	// Startup inline script, which initializes the window scaling and loads the app.
+	startup: (screenTypes, jsAssets) => fn(startup(screenTypes, jsAssets)),
+	// Update inline script, which updates the template/prerender content prior to app render.
+	// Used for locale and deeplinking customizations.
+	update: (mapping, deep) => fn(deepLink(deep, multiLocale(mapping)))
+};
