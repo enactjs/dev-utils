6ea35069305268c046a60ab21daed73a09d80719
Jason Robitaille <robitaille88@gmail.com>
2018-03-06 13:52:23 -0800

    Ensure moonstone resbundle is correctly detected when running tests within moonstone itself.

--- plugins/ILibPlugin/index.js
+++ plugins/ILibPlugin/index.js
@@ -161,9 +161,9 @@ function emitAsset(compilation, name, data) {
 function ILibPlugin(options) {
 	this.options = options || {};
 	this.options.ilib = this.options.ilib || process.env.ILIB_BASE_PATH;
+	const pkgName = packageName('./package.json');
 	if (typeof this.options.ilib === 'undefined') {
 		try {
-			const pkgName = packageName('./package.json');
 			if (pkgName.indexOf('@enact') === 0) {
 				this.options.resources = false;
 			}
@@ -182,9 +182,13 @@ function ILibPlugin(options) {
 	}
 	this.options.bundles = this.options.bundles || {};
 	if (typeof this.options.bundles.moonstone === 'undefined') {
-		const moonstone = packageSearch(process.cwd(), '@enact/moonstone');
-		if (moonstone) {
-			this.options.bundles.moonstone = path.join(moonstone, 'resources');
+		if (pkgName === '@enact/moonstone') {
+			this.options.bundles.moonstone = 'resources';
+		} else {
+			const moonstone = packageSearch(process.cwd(), '@enact/moonstone');
+			if (moonstone) {
+				this.options.bundles.moonstone = path.join(moonstone, 'resources');
+			}
 		}
 	}
 
