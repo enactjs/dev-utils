15bdd66454f1395711c14f4130ca5b0255a98dc5
Ryan Duffy <ryan.duffy@lge.com>
2018-09-19 15:56:44 -0700

    fix unmanaged to set correct uglify props
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- mixins/unmangled.js
+++ mixins/unmangled.js
@@ -5,9 +5,9 @@ module.exports = {
 		// Allow Uglify's optimizations/debug-code-removal but don't minify
 		const uglifyPlugin = helper.getPluginByName(config, 'UglifyJsPlugin');
 		if (uglifyPlugin) {
-			uglifyPlugin.options.mangle = false;
-			uglifyPlugin.options.beautify = true;
-			uglifyPlugin.options.output.comments = true;
+			uglifyPlugin.options.uglifyOptions.mangle = false;
+			uglifyPlugin.options.uglifyOptions.beautify = true;
+			uglifyPlugin.options.uglifyOptions.output.comments = true;
 			config.output.pathinfo = true;
 		}
 	}
