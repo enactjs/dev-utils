39003664e906695296ead51fe2b36f7cda5ad1e7
Jason Robitaille <jason.robitaille@lge.com>
2018-10-02 16:10:14 -0700

    Fix unmangling output for TerserPlugin

--- mixins/unmangled.js
+++ mixins/unmangled.js
@@ -2,12 +2,12 @@ const helper = require('../config-helper');
 
 module.exports = {
 	apply: function(config) {
-		// Allow Uglify's optimizations/debug-code-removal but don't minify
-		const uglifyPlugin = helper.getPluginByName(config, 'UglifyJsPlugin');
-		if (uglifyPlugin) {
-			uglifyPlugin.options.uglifyOptions.mangle = false;
-			uglifyPlugin.options.uglifyOptions.beautify = true;
-			uglifyPlugin.options.uglifyOptions.output.comments = true;
+		// Allow Terser's optimizations/debug-code-removal but don't minify
+		const terserPlugin = helper.getMinimizerByName(config, 'TerserPlugin');
+		if (terserPlugin) {
+			terserPlugin.options.terserOptions.mangle = false;
+			terserPlugin.options.terserOptions.output.beautify = true;
+			terserPlugin.options.terserOptions.output.comments = true;
 			config.output.pathinfo = true;
 		}
 	}
