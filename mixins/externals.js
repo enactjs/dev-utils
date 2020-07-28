d648f515607a34a1fc6a1d22a78f05fa7cc94c1c
Jason Robitaille <robitaille88@gmail.com>
2020-07-28 16:42:05 -0700

    Expand framework build scope to include local-scoped theme files and optionally polyfills/core-js.

--- mixins/externals.js
+++ mixins/externals.js
@@ -1,16 +1,31 @@
+const fs = require('fs');
+const path = require('path');
 const helper = require('../config-helper');
+const packageRoot = require('../package-root');
 const EnactFrameworkRefPlugin = require('../plugins/dll/EnactFrameworkRefPlugin');
 
 module.exports = {
 	apply: function (config, opts = {}) {
+		// Scan for any polyfills.js entry file for potential delegate.
+		const polyfill = helper.polyfillFile(config);
+
 		// Include plugin to hook into its events
 		const htmlPluginInstance = helper.getPluginByName(config, 'HtmlWebpackPlugin');
+
+		const libraries = ['@enact', 'react', 'react-dom', 'ilib'];
+		if (opts['externals-corejs'] || opts.externalsCorejs) libraries.push('core-js');
+
+		const app = packageRoot();
+		if (app.meta.name.startsWith('@enact/') && fs.existsSync(path.join(app.path, 'ThemeDecorator'))) {
+			libraries.push('.');
+		}
 
 		// Add the reference plugin so the app uses the external framework
 		config.plugins.push(
 			new EnactFrameworkRefPlugin({
 				name: 'enact_framework',
-				libraries: ['@enact', 'react', 'react-dom', 'ilib'],
+				libraries,
+				polyfill,
 				external: {
 					publicPath: opts['externals-public'] || opts.externalsPublic || opts.externals,
 					snapshot: opts.snapshot
