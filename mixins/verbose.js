5b43d31bdd3472a9f76743a2d177db83416b04af
Mikyung Kim <mikyung27.kim@lge.com>
2022-03-29 17:40:56 +0900

    WRN-19252: Use ProgressPlugin from originated place
    Enact-DCO-1.0-Signed-off-by: Mikyung Kim (mikyung27.kim@lge.com)

--- mixins/verbose.js
+++ mixins/verbose.js
@@ -2,13 +2,14 @@ const helper = require('../config-helper');
 const VerboseLogPlugin = require('../plugins/VerboseLogPlugin');
 
 module.exports = {
-	apply: function (config) {
+	apply: function (config, opts = {}) {
 		const prerenderInstance = helper.getPluginByName(config, 'PrerenderPlugin');
 		const snapshotPluginInstance = helper.getPluginByName(config, 'SnapshotPlugin');
 
 		return config.plugins.push(
 			new VerboseLogPlugin({
 				prerenderPlugin: prerenderInstance && prerenderInstance.constructor,
+				ProgressPlugin: opts.ProgressPlugin,
 				snapshotPlugin: snapshotPluginInstance && snapshotPluginInstance.constructor
 			})
 		);
