bd6cf0e841719641f8caf02fdc47b3de71e6048c
taeyoung.hong <35059065+hong6316@users.noreply.github.com>
2022-07-05 16:25:34 +0900

    revert WRN-19252 and use compiler.webpack.ProgressPlugin itself (#106)

--- mixins/verbose.js
+++ mixins/verbose.js
@@ -2,14 +2,13 @@ const helper = require('../config-helper');
 const VerboseLogPlugin = require('../plugins/VerboseLogPlugin');
 
 module.exports = {
-	apply: function (config, opts = {}) {
+	apply: function (config) {
 		const prerenderInstance = helper.getPluginByName(config, 'PrerenderPlugin');
 		const snapshotPluginInstance = helper.getPluginByName(config, 'SnapshotPlugin');
 
 		return config.plugins.push(
 			new VerboseLogPlugin({
 				prerenderPlugin: prerenderInstance && prerenderInstance.constructor,
-				ProgressPlugin: opts.ProgressPlugin,
 				snapshotPlugin: snapshotPluginInstance && snapshotPluginInstance.constructor
 			})
 		);
