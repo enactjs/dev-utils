6655e041da2c4ea3dec3bfb0d54ddafd769ffabd
Mikyung Kim <mikyung27.kim@lge.com>
2022-07-05 10:32:23 +0900

    Update if statement of checking NodeOutputFileSystem
    Enact-DCO-1.0-Signed-off-by: Mikyung Kim (mikyung27.kim@lge.com)

--- plugins/GracefulFsPlugin/index.js
+++ plugins/GracefulFsPlugin/index.js
@@ -9,11 +9,7 @@ class GracefulFsPlugin {
 
 	apply(compiler) {
 		compiler.hooks.afterEnvironment.tap('GracefulFsPlugin', () => {
-			if (
-				compiler.outputFileSystem &&
-				compiler.outputFileSystem.constructor &&
-				compiler.outputFileSystem.constructor.name === 'NodeOutputFileSystem'
-			) {
+			if (compiler.outputFileSystem && JSON.stringify(compiler.outputFileSystem) === JSON.stringify(fs)) {
 				for (let i = 0; i < replaceable.length; i++) {
 					if (this.options[replaceable[i]] && compiler.outputFileSystem[replaceable[i]]) {
 						compiler.outputFileSystem[replaceable[i]] = fs[replaceable[i]].bind(fs);
